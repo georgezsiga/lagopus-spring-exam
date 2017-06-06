d4185871f3598735cb11e06c3b11fd1938294d44
georgezsiga <georgezsiga@gmail.com>
2017-06-06 10:16:40 +0200

    Add questions to database

--- src/main/java/com/greenfox/spring/SpringexamApplication.java
+++ src/main/java/com/greenfox/spring/SpringexamApplication.java
@@ -1,5 +1,6 @@
 package com.greenfox.spring;
 
+import com.greenfox.spring.Model.Question;
 import com.greenfox.spring.Repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -18,6 +19,13 @@ public class SpringexamApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-
+    questionRepository.save(new Question("When did your course start? (yyyy.mm.dd)"));
+    questionRepository.save(new Question("What type of dog Barbi has?"));
+    questionRepository.save(new Question("What is HerrNorbert's favourite color?"));
+    questionRepository.save(new Question("How many classes are learning at Green Fox Academy at this moment?"));
+    questionRepository.save(new Question("How many mentors teach at Green Fox at this moment?"));
+    questionRepository.save(new Question("What was the name of the first Green Fox class?"));
+    questionRepository.save(new Question("How many likes do we have on facebook?"));
+    questionRepository.save(new Question("What is Tojas's horoscope?"));
 	}
 }
