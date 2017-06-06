c17637dcbdd4df7319b48c6fe05f779486f3aeb0
georgezsiga <georgezsiga@gmail.com>
2017-06-06 10:21:10 +0200

    add autowired answerrepository

--- src/main/java/com/greenfox/spring/SpringexamApplication.java
+++ src/main/java/com/greenfox/spring/SpringexamApplication.java
@@ -1,6 +1,7 @@
 package com.greenfox.spring;
 
 import com.greenfox.spring.Model.Question;
+import com.greenfox.spring.Repository.AnswerRepository;
 import com.greenfox.spring.Repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,11 +14,16 @@ public class SpringexamApplication implements CommandLineRunner{
 	@Autowired
 	QuestionRepository questionRepository;
 
+	@Autowired
+  AnswerRepository answerRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringexamApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+	  
+
 	}
 }
