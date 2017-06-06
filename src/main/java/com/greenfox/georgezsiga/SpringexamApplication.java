513012dfa0a74b68e21722bddacc4860699eafde
georgezsiga <georgezsiga@gmail.com>
2017-06-06 09:34:56 +0200

    Add initial files

---
+++ src/main/java/com/greenfox/georgezsiga/SpringexamApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.georgezsiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringexamApplication.class, args);
+	}
+}
