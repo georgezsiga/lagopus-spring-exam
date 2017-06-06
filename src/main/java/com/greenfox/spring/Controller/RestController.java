2cdaf438c22743a591e0b043e46d6694db7d4546
georgezsiga <georgezsiga@gmail.com>
2017-06-06 11:25:53 +0200

    Add post endpoint

--- src/main/java/com/greenfox/spring/Controller/RestController.java
+++ src/main/java/com/greenfox/spring/Controller/RestController.java
@@ -1,9 +1,12 @@
 package com.greenfox.spring.Controller;
 
+import com.greenfox.spring.Model.Answers;
 import com.greenfox.spring.Model.Questions;
+import com.greenfox.spring.Repository.AnswerRepository;
 import com.greenfox.spring.Repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by georgezsiga on 6/6/17.
@@ -14,9 +17,17 @@ public class RestController {
   @Autowired
   private QuestionRepository questionRepository;
 
+  @Autowired
+  private AnswerRepository answerRepository;
+
   @GetMapping("/questions")
   public Questions questions() {
     return new Questions(questionRepository.findAll());
+  }
+
+  @PostMapping("/answers")
+  public Answers answers() {
+    return new Answers(answerRepository.findAll());
   }
 
 }
