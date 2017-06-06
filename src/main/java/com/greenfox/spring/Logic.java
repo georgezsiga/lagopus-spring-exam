f502f9f50fdc96309f744d0f08ca57a5d62824bc
georgezsiga <georgezsiga@gmail.com>
2017-06-06 13:12:52 +0200

    add randomquestion method

--- src/main/java/com/greenfox/spring/Logic.java
+++ src/main/java/com/greenfox/spring/Logic.java
@@ -4,6 +4,7 @@ import com.greenfox.spring.Model.Answer;
 import com.greenfox.spring.Model.Answers;
 import com.greenfox.spring.Model.Questions;
 import com.greenfox.spring.Repository.AnswerRepository;
+import com.greenfox.spring.Repository.QuestionRepository;
 import java.util.ArrayList;
 import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,9 @@ public class Logic {
 
   @Autowired
   AnswerRepository answerRepository;
+
+  @Autowired
+  QuestionRepository questionRepository;
 
   ArrayList<Answer> rightAnswers = new ArrayList<Answer>();
 
@@ -33,4 +37,18 @@ public class Logic {
     }
   }
 
+  public ArrayList<com.greenfox.spring.Model.Question> getRandomQuestions() {
+    ArrayList<com.greenfox.spring.Model.Question> allQuestions = (ArrayList<com.greenfox.spring.Model.Question>) questionRepository.findAllBy();
+    ArrayList<com.greenfox.spring.Model.Question> randomQuestions = new ArrayList<com.greenfox.spring.Model.Question>();
+    Long length = Long.valueOf(allQuestions.size());
+    while (randomQuestions.size() < 5) {
+      randomQuestions.add(questionRepository.findOne(randomNumber(length)));
+    }
+    return randomQuestions;
+  }
+
+  public Long randomNumber(Long totalNumberOfQuestions) {
+    Long randNum = Long.valueOf((int) Math.random() * totalNumberOfQuestions + 1);
+    return randNum;
+  }
 }
