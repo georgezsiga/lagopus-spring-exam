149ac2db9d2a33c8c49f10cdccbc31b246dcbea9
georgezsiga <georgezsiga@gmail.com>
2017-06-06 10:17:20 +0200

    remove new questons from adding them

--- src/main/java/com/greenfox/spring/SpringexamApplication.java
+++ src/main/java/com/greenfox/spring/SpringexamApplication.java
@@ -19,13 +19,5 @@ public class SpringexamApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-    questionRepository.save(new Question("When did your course start? (yyyy.mm.dd)"));
-    questionRepository.save(new Question("What type of dog Barbi has?"));
-    questionRepository.save(new Question("What is HerrNorbert's favourite color?"));
-    questionRepository.save(new Question("How many classes are learning at Green Fox Academy at this moment?"));
-    questionRepository.save(new Question("How many mentors teach at Green Fox at this moment?"));
-    questionRepository.save(new Question("What was the name of the first Green Fox class?"));
-    questionRepository.save(new Question("How many likes do we have on facebook?"));
-    questionRepository.save(new Question("What is Tojas's horoscope?"));
 	}
 }
