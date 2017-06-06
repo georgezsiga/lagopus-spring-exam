15c22485b11b3f1b602cf204e426a81943c69827
georgezsiga <georgezsiga@gmail.com>
2017-06-06 09:41:23 +0200

    Add initial file

---
+++ src/main/java/com/greenfox/spring/Controller/RestController.java
@@ -0,0 +1,9 @@
+package com.greenfox.spring.Controller;
+
+/**
+ * Created by georgezsiga on 6/6/17.
+ */
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+}
