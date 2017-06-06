89fcb008ad28307f33fa339f36ad4be2f5af3247
georgezsiga <georgezsiga@gmail.com>
2017-06-06 10:20:20 +0200

    add initial file

---
+++ src/main/java/com/greenfox/spring/Repository/AnswerRepository.java
@@ -0,0 +1,11 @@
+package com.greenfox.spring.Repository;
+
+import com.greenfox.spring.Model.Answer;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by georgezsiga on 6/6/17.
+ */
+public interface AnswerRepository extends CrudRepository<Answer, Long> {
+
+}
