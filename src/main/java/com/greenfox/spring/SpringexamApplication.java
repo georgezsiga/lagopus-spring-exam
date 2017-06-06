c48cf805deef979a891e96f6f2a874309c968c4a
georgezsiga <georgezsiga@gmail.com>
2017-06-06 09:38:15 +0200

    Add implement commandlineRunner and its method

--- src/main/java/com/greenfox/spring/SpringexamApplication.java
+++ src/main/java/com/greenfox/spring/SpringexamApplication.java
@@ -1,12 +1,18 @@
-package com.greenfox.georgezsiga;
+package com.greenfox.spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringexamApplication {
+public class SpringexamApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringexamApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 }
