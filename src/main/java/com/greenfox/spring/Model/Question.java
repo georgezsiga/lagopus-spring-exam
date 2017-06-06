305a4f658cdac0b2b8b0e0043c858df9d2c850e9
georgezsiga <georgezsiga@gmail.com>
2017-06-06 10:00:58 +0200

    Add fields, constructors and getters setters

--- src/main/java/com/greenfox/spring/Model/Question.java
+++ src/main/java/com/greenfox/spring/Model/Question.java
@@ -1,8 +1,42 @@
 package com.greenfox.spring.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by georgezsiga on 6/6/17.
  */
+@Entity
 public class Question {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  long id;
+
+  String question;
+
+  public Question() {
+  }
+
+  public Question(String question) {
+    this.question = question;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getQuestion() {
+    return question;
+  }
+
+  public void setQuestion(String question) {
+    this.question = question;
+  }
 }
