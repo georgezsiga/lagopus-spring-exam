ea94fca8ac67411b5c46532fc8b215604a214ed7
georgezsiga <georgezsiga@gmail.com>
2017-06-06 13:13:26 +0200

    add method

--- src/main/java/com/greenfox/spring/Repository/AnswerRepository.java
+++ src/main/java/com/greenfox/spring/Repository/AnswerRepository.java
@@ -2,10 +2,13 @@ package com.greenfox.spring.Repository;
 
 import com.greenfox.spring.Model.Answer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by georgezsiga on 6/6/17.
  */
 public interface AnswerRepository extends CrudRepository<Answer, Long> {
+
+  Answer findFirstById(@Param("id") Long id);
 
 }
