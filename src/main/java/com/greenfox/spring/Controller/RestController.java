233a66e34d9c05ea4a1e97fa5ea5b72aecafd7c3
georgezsiga <georgezsiga@gmail.com>
2017-06-06 09:50:37 +0200

    Add get endpoint

--- src/main/java/com/greenfox/spring/Controller/RestController.java
+++ src/main/java/com/greenfox/spring/Controller/RestController.java
@@ -1,9 +1,16 @@
 package com.greenfox.spring.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * Created by georgezsiga on 6/6/17.
  */
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+  @GetMapping("/questions")
+  public Questions questions() {
+    return questions;
+  }
 
 }
