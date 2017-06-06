c2b7b5bdc05bef281b2ed0fa6b27b105fc34a121
georgezsiga <georgezsiga@gmail.com>
2017-06-06 12:16:54 +0200

    add initial file

---
+++ src/test/java/com/greenfox/spring/Controller/RestControllerTest.java
@@ -0,0 +1,20 @@
+package com.greenfox.spring.Controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Created by georgezsiga on 6/6/17.
+ */
+public class RestControllerTest {
+
+  @Test
+  public void questions() throws Exception {
+  }
+
+  @Test
+  public void answers() throws Exception {
+  }
+
+}
