6ce908686fed7b60e274622e8ece428b93153de3
georgezsiga <georgezsiga@gmail.com>
2017-06-06 09:52:55 +0200

    Add initial file with fields

---
+++ src/main/java/com/greenfox/spring/Model/Questions.java
@@ -0,0 +1,13 @@
+package com.greenfox.spring.Model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by georgezsiga on 6/6/17.
+ */
+public class Questions {
+
+  Long id;
+  ArrayList<Question> questions;
+
+}
