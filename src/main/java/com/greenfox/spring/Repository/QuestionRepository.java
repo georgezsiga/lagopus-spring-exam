c4ae75132f3b3cbf4e46f0edaed73413efff3742
georgezsiga <georgezsiga@gmail.com>
2017-06-06 11:01:31 +0200

    add query methods

--- src/main/java/com/greenfox/spring/Repository/QuestionRepository.java
+++ src/main/java/com/greenfox/spring/Repository/QuestionRepository.java
@@ -1,11 +1,19 @@
 package com.greenfox.spring.Repository;
 
 import com.greenfox.spring.Model.Question;
+import java.util.ArrayList;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by georgezsiga on 6/6/17.
  */
 public interface QuestionRepository extends CrudRepository<Question, Long> {
+
+  @Query ("SELECT question FROM Question ORDER BY RAND() LIMIT 5")
+  ArrayList<Question> findAllByQuestion();
+
+  ArrayList<Question> findTop5ByQuestion();
+
 
 }
